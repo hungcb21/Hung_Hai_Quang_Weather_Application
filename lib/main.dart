1fe0594313e3cfc55a3afedf9df290b4e9e4cb6c
Cao Bá Hưng <hungcb21@gmail.com>
2022-01-07 07:15:29 +0700

    create project

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/src/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
---
+++ lib/src/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/src/screen/main_screen/main_screen.dart';
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:MainScreen(),
+    );
+  }
+}
