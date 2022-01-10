42891760383c8ef73676f981c940c27192ad2ab3
vanquangpham165 <phamvanquang.work@gmail.com>
2022-01-10 20:55:46 +0700

    update

--- lib/src/models/city.dart
+++ lib/src/models/city.dart
@@ -4,21 +4,42 @@ class City extends Equatable {
   final num id;
   final String name;
   final String country;
+  final Coordinate coordinate;
 
-  const City({
-    required this.id,
-    required this.name,
-    required this.country,
-  });
+  const City(
+      {required this.id,
+      required this.name,
+      required this.country,
+      required this.coordinate});
 
   factory City.fromJson(Map<String, dynamic> json) {
     return City(
-      id: json['id'],
-      name: json['name'],
-      country: json['country'],
-    );
+        id: json['id'],
+        name: json['name'],
+        country: json['country'],
+        coordinate: json['coord']);
   }
 
   @override
   List<Object?> get props => [id, name, country];
+}
+
+class Coordinate extends Equatable {
+  final num longitude;
+  final num latitude;
+
+  const Coordinate({
+    required this.longitude,
+    required this.latitude,
+  });
+
+  factory Coordinate.fromJson(Map<String, dynamic> json) {
+    return Coordinate(
+      longitude: json['lon'],
+      latitude: json['lat'],
+    );
+  }
+
+  @override
+  List<Object?> get props => [longitude, latitude];
 }
